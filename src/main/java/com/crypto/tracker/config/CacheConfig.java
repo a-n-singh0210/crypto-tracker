ed9bb21a17fc5bc7eb6cdb11e234d8f722106685
a-n-singh0210 <a.n.singh0210@gmail.com>
2026-04-25 19:19:02 +0530

    Fix syntax error: remove hyphen from caffeine package name

--- src/main/java/com/crypto/tracker/config/CacheConfig.java
+++ src/main/java/com/crypto/tracker/config/CacheConfig.java
@@ -1,6 +1,6 @@
 package com.crypto.tracker.config;
 
-import com.github.ben-manes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
