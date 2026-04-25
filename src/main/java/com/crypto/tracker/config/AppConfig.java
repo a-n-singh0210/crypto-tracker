a65039b0120caafd3a850cb9d2b5e510a598c83a
a-n-singh0210 <a.n.singh0210@gmail.com>
2026-04-25 19:22:58 +0530

    Fix duplicate bean error: merge Caffeine config into AppConfig

--- src/main/java/com/crypto/tracker/config/AppConfig.java
+++ src/main/java/com/crypto/tracker/config/AppConfig.java
@@ -3,10 +3,11 @@ package com.crypto.tracker.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
@@ -20,6 +21,10 @@ public class AppConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("prices", "market", "predictions", "historical");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("prices", "market", "predictions", "historical");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(5, TimeUnit.MINUTES) // Fresh data every 5 minutes
+                .maximumSize(500));
+        return cacheManager;
     }
 }
